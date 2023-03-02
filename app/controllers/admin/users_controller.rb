403e9e94532a9f1b2de7931c7dc8f03f75e96990
Danh <tqdanh@tma.com.vn>
2023-03-02 16:01:00 +0700

    admin create new user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,24 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to edit_admin_user_path @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :role_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   belongs_to :role
+
+  validates :username, presence: true, length: { minimum: 5 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
