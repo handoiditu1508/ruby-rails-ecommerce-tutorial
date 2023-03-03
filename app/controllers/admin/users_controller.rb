771c0a906977d0cb454398444234291f39811dfa
Danh <tqdanh@tma.com.vn>
2023-03-03 09:42:35 +0700

    admin edit user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,9 +11,23 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to edit_admin_user_path @user
+      redirect_to edit_admin_user_path(@user)
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_admin_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
